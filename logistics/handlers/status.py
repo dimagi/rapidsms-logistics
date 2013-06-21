aea985bdb59fb2c79e397c3bdb0424ba21f84472
rowenaluk <rluk@dimagi.com>
2013-06-21 14:14:37 -0400

    small fix to report properly when facility not set properlyw

--- logistics/handlers/status.py
+++ logistics/handlers/status.py
@@ -22,7 +22,10 @@ class Stop(KeywordHandler):
             return
         if config.Responsibilities.REPORTEE_RESPONSIBILITY in self.msg.contact.role.responsibilities.values_list('code', flat=True):
             # if a super, show last report from your facility
-            reports = ProductReport.objects.filter(facility=self.msg.contact.facility)
+            if self.msg.contact.supply_point is None:
+                self.respond("Your are not registered to a facility. Please contact your DHIO or RHIO for assistance.")
+                return
+            reports = ProductReport.objects.filter(supply_point=self.msg.contact.supply_point)
             if not reports:
                 self.respond("Your facility has not submitted any stock reports yet.")
                 return
@@ -35,6 +38,14 @@ class Stop(KeywordHandler):
                 return
             resp = "You sent '%(report)s' on %(date)s "
         last_report = reports.order_by("-report_date")[0]
+        if last_report.message is None:
+            resp = "Last report:  %(quantity)s %(product)s %(type)s at %(facility)s, submitted on %(date)s from the website ewsghana.com"
+            self.respond(resp % {'quantity': last_report.quantity, 
+                                 'product': last_report.product, 
+                                 'type': last_report.report_type, 
+                                 'facility': last_report.supply_point, 
+                                 'date': last_report.report_date.strftime('%h %d')})
+            return
         self.respond(resp % {'report': last_report.message.text, 
                               'date': last_report.report_date.strftime('%h %d')})
 
